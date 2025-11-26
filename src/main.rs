e2517d38c03edea91343028ce0330c5fffd0764e
frost-head <ed.ayush2003@gmail.com>
2025-11-26 22:51:09 +0530

    basic lexer funcitonality. It can read single char

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+use sutra::token::{Token, TokenKind};
+use sutra::lexer::Lexer;
+
 fn main() {
-    let firsttoken = sutra::Token {
+    let firsttoken = Token {
         literal: String::from("2"),
-        kind: sutra::TokenKind::LeftParen,
+        kind: TokenKind::LeftParen,
     };
+    let mut lexer = Lexer::new(String::from("()[]{}"));
+    let tok =  lexer.next();
+    println!("{:?}", tok);
+    let tok = match tok {
+        Some(token) => token,
+        None => panic!("Unexpected end of input"),
+    };
+    println!("Printing token :: {:?}", tok);
+    let perfect = Token::new(TokenKind::LeftParen, String::from("("));
+    
+    println!("Printing perfect token :: {:?}", perfect);
     println!("Frist token {:#?}", firsttoken);
 }
