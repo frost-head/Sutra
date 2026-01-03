0c9beff7795dc2a19618fe7bfc035cf73875344d
frost-head <ed.ayush2003@gmail.com>
2026-01-03 11:13:35 +0530

    Issue #7 : Refactor AST parsing to handle numbers and semicolons
    Add support for parsing number literals and handling semicolons in
    expressions. Update imports and error handling accordingly.

--- src/ast/expression/parse.rs
+++ src/ast/expression/parse.rs
@@ -1,5 +1,10 @@
-use crate::{ast::expression::Expression, errors::ParserError, parser::Parser};
-use anyhow::Result;
+use crate::{
+    ast::expression::Expression,
+    errors::ParserError,
+    lexer::token::{PuncuationKind, Token},
+    parser::Parser,
+};
+use anyhow::{Ok, Result};
 
 impl Expression {
     pub fn parse(parser: &mut Parser) -> Result<Expression> {
@@ -9,8 +14,35 @@ impl Expression {
             // Token::Operator(operator_kind) => todo!(),
             // Token::Punctuation(puncuation_kind) => todo!(),
             // Token::Ident(_) => todo!(),
-            // Token::Number(_) => todo!(),
-            _ => return Err(ParserError::UnexpectedToken { token: new.clone() }.into()),
+            Token::Number(num) => {
+                let val = num.clone();
+                parser.consume()?;
+                let peek = parser.peek()?;
+                println!("{}", peek);
+                match peek {
+                    Token::Punctuation(punctuation_kind) => match punctuation_kind {
+                        PuncuationKind::SemiColon => {
+                            parser.consume()?;
+                            return Ok(Expression::Literal { literal: val });
+                        }
+                        _ => {
+                            return Err(ParserError::UnexpectedToken {
+                                token: peek.clone(),
+                            }
+                            .into());
+                        }
+                    },
+                    _ => {
+                        return Err(ParserError::UnexpectedToken {
+                            token: peek.clone(),
+                        }
+                        .into());
+                    }
+                }
+            }
+            _ => {
+                return Err(ParserError::UnexpectedToken { token: new.clone() }.into());
+            }
         }
     }
 }
